8b8a52fd9c60a9d980e5590c24457d348d7860c9
Jiwon3114 <jw000jw@gmail.com>
2025-08-08 17:49:50 +0900

    Borad 1.2

---
+++ src/main/java/board/board/controller/BoardController.java
@@ -0,0 +1,13 @@
+package board.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BoardController {
+
+	@RequestMapping("/Board/List")
+	public String list() {
+		return "";
+	}
+}
---
+++ src/main/java/com/board/controller/BoradDTO.java
@@ -0,0 +1,28 @@
+package com.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class BoradDTO {
+	// 필드
+	private int idx;
+	private string menu_id;
+	private string title;
+	private string content;
+	private string writer;
+	private string regdate;
+	private int hit;
+	
+	
+	}
+	
+	
+
+
+
+
+
+
